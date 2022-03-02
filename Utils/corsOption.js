a0f9d0b066ca5372698fc0f59fe44933995f1c3d
alexshady008 <alexshady008.2@gmail.com>
2022-03-02 06:03:25 -0300

    Opciones del cors 3

--- Utils/corsOption.js
+++ Utils/corsOption.js
@@ -1,4 +1,4 @@
-const whitelist = ['http://localhost:3000', 'https://fronttrain.com.ar']
+const whitelist = ['http://localhost:3000', 'http://localhost:3000/product', 'https://fronttrain.com.ar']
 const options = {
   origin: (origin, cb) => {
     if (whitelist.includes(origin) || !origin) {
